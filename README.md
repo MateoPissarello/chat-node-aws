c768830e964bf66910b93687bd6fbffb70231f20
Mateo Pissarello <mateopissa@gmail.com>
2023-12-18 17:10:00 -0500

    Development of Chat Server Administration API

---
+++ README.md
@@ -0,0 +1,68 @@
+# Serverless - AWS Node.js Typescript
+
+Serverless Framework template for zero-config TypeScript support.
+
+## Features
+
+Thanks to [`serverless-typescript`](https://github.com/prisma-labs/serverless-plugin-typescript) plugin:
+
+- Zero-config: Works out of the box without the need to install any other compiler or plugins
+- Supports ES2015 syntax + features (`export`, `import`, `async`, `await`, `Promise`, ...)
+- Supports `sls package`, `sls deploy` and `sls deploy function`
+- Supports `sls invoke local` + `--watch` mode
+- Integrates nicely with [`serverless-offline`](https://github.com/dherault/serverless-offline)
+
+## Prerequisites
+
+- [`serverless-framework`](https://github.com/serverless/serverless)
+- [`node.js`](https://nodejs.org)
+
+## Usage
+
+To create new serverless AWS TypeScript project using this template run:
+
+```bash
+serverless create \
+--template-url https://github.com/ttarnowski/serverless-aws-nodejs-typescript/tree/main \
+--path myServiceName
+```
+
+where `myServiceName` should be replaced with the name of your choice.
+
+Then change directory to the newly created one:
+
+```
+cd myServiceName
+```
+
+And run:
+
+```
+npm install
+```
+
+or:
+
+```
+yarn install
+```
+
+## Licence
+
+MIT.
+
+
+{"action":"sendMessage","message":"Hola Primo","recipientNickname":"Mateo"}
+
+serverless deploy function --function websocketHandler
+
+
+install nvm
+https://github.com/coreybutler/nvm-windows/releases
+nvm install 14
+nvm use 14
+nvm list
+
+
+
+npm install --global windows-build-tools
---
+++ src/handler.ts
@@ -0,0 +1,315 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyEventQueryStringParameters,
+  APIGatewayProxyResult,
+} from "aws-lambda";
+import AWS, { AWSError } from "aws-sdk";
+import { Key } from "aws-sdk/clients/dynamodb";
+import { get } from "http";
+import { v4 } from "uuid";
+type Action = "$connect" | "$disconnect" | "getMessages" | "sendMessage" | "getClients";
+type Client = {
+  connectionId: string;
+  nickname: string;
+};
+
+type GetMessagesBody = {
+  targetNickname: string;
+  limit: number;
+  startKey: Key | undefined;
+};
+
+type SendMessageBody = {
+  message: string;
+  recipientNickname: string;
+};
+const docClient = new AWS.DynamoDB.DocumentClient();
+const apiGw = new AWS.ApiGatewayManagementApi({
+  endpoint: process.env["WSSAPIGATEWAYENDPOINT"],
+});
+
+const responseOK = {
+  statusCode: 200,
+  body: "",
+};
+
+const responseForbidden = {
+  statusCode: 403,
+  body: "",
+};
+class HandlerError extends Error {}
+const CLIENT_TABLE_NAME = "Clients";
+const MESSAGES_TABLE_NAME = "Messages";
+export const handle = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  // wss://asds.asw.com?nickname = abc
+  const connectionId = event.requestContext.connectionId as string;
+  const routeKey = event.requestContext.routeKey as Action;
+  console.log("this is a route key ", routeKey);
+  console.log("this a connection id ", connectionId);
+  try {
+    switch (routeKey) {
+      case "$connect":
+        return handleConnect(connectionId, event.queryStringParameters);
+      case "$disconnect":
+        return handleDisconnect(connectionId);
+      case "getClients":
+        return handleGetClients(connectionId);
+      case "sendMessage":
+        return handleSendMessage(connectionId, parseSendMessageBody(event.body));
+      case "getMessages":
+        return handleGetMessages(connectionId, parseGetMessagesBody(event.body));
+      default:
+        return {
+          statusCode: 500,
+          body: "",
+        };
+    }
+  } catch (e) {
+    if (e instanceof HandlerError) {
+      await postToConnection(connectionId, JSON.stringify({ type: "error", message: e.message }));
+      return responseOK;
+    }
+
+    throw e;
+  }
+};
+
+const parseSendMessageBody = (body: string | null): SendMessageBody => {
+  const sendMessageBody = JSON.parse(body || "{}") as SendMessageBody;
+
+  if (
+    !sendMessageBody ||
+    typeof sendMessageBody.message !== "string" ||
+    typeof sendMessageBody.recipientNickname !== "string"
+  ) {
+    throw new HandlerError("Incorrect SendMessageBody format");
+  }
+  return sendMessageBody;
+};
+
+const parseGetMessagesBody = (body: string | null): GetMessagesBody => {
+  const getMessagesBody = JSON.parse(body || "{}") as GetMessagesBody;
+
+  if (
+    !getMessagesBody ||
+    typeof getMessagesBody.targetNickname !== "string" ||
+    typeof getMessagesBody.limit !== "number"
+  ) {
+    throw new HandlerError("Incorrect GetMessagesBody format");
+  }
+  return getMessagesBody;
+};
+
+const handleConnect = async (
+  connectionId: string,
+  queryParams: APIGatewayProxyEventQueryStringParameters | null,
+): Promise<APIGatewayProxyResult> => {
+  if (!queryParams || !queryParams["nickname"]) {
+    return responseForbidden;
+  }
+
+  const existingConnectionId = await getConnectionIdByNickname(queryParams["nickname"]);
+  if (
+    existingConnectionId &&
+    (await postToConnection(existingConnectionId, JSON.stringify({ type: "ping" })))
+  ) {
+    return responseForbidden;
+  }
+
+  await docClient
+    .put({
+      TableName: CLIENT_TABLE_NAME,
+      Item: {
+        connectionId,
+        nickname: queryParams["nickname"],
+      },
+    })
+    .promise();
+
+  await notifyClients(connectionId);
+
+  return responseOK;
+};
+
+const getConnectionIdByNickname = async (nickname: string): Promise<string | undefined> => {
+  const output = await docClient
+    .query({
+      TableName: CLIENT_TABLE_NAME,
+      IndexName: "NicknameIndex",
+      KeyConditionExpression: "#nickname = :nickname",
+      ExpressionAttributeNames: {
+        "#nickname": "nickname",
+      },
+      ExpressionAttributeValues: {
+        ":nickname": nickname,
+      },
+    })
+    .promise();
+
+  if (output.Count && output.Count > 0) {
+    const client = (output.Items as Client[])[0];
+    return client.connectionId;
+  }
+  return undefined;
+};
+
+const handleDisconnect = async (connectionId: string): Promise<APIGatewayProxyResult> => {
+  await docClient
+    .delete({
+      TableName: CLIENT_TABLE_NAME,
+      Key: {
+        connectionId,
+      },
+    })
+    .promise();
+
+  await notifyClients(connectionId);
+
+  return responseOK;
+};
+
+const getAllClients = async (): Promise<Client[]> => {
+  const output = await docClient
+    .scan({
+      TableName: CLIENT_TABLE_NAME,
+    })
+    .promise();
+
+  const clients = output.Items || [];
+
+  return clients as Client[];
+};
+
+const notifyClients = async (connectionIdToExclude: string) => {
+  const clients = await getAllClients();
+
+  await Promise.all(
+    clients
+      .filter((client) => client.connectionId !== connectionIdToExclude)
+      .map(async (client) => {
+        await postToConnection(client.connectionId, createClientsMessage(clients));
+      }),
+  );
+};
+const postToConnection = async (connectionId: string, data: string): Promise<boolean> => {
+  try {
+    await apiGw
+      .postToConnection({
+        ConnectionId: connectionId,
+        Data: data,
+      })
+      .promise();
+
+    return true;
+  } catch (e) {
+    if ((e as AWSError).statusCode !== 410) {
+      throw e;
+    }
+
+    await docClient
+      .delete({
+        TableName: CLIENT_TABLE_NAME,
+        Key: {
+          connectionId,
+        },
+      })
+      .promise();
+
+    return false;
+  }
+};
+const handleGetClients = async (connectionId: string): Promise<APIGatewayProxyResult> => {
+  const clients = await getAllClients();
+  await postToConnection(connectionId, createClientsMessage(clients));
+  return responseOK;
+};
+
+const createClientsMessage = (clients: Client[]): string =>
+  JSON.stringify({ type: "clients", value: clients });
+
+const handleSendMessage = async (
+  senderConnectionId: string,
+  body: SendMessageBody,
+): Promise<APIGatewayProxyResult> => {
+  const senderClient = await getClient(senderConnectionId);
+  console.log("this is a body ", body);
+  const nicknameTonickname = getNicknameToNickname([senderClient.nickname, body.recipientNickname]);
+  await docClient
+    .put({
+      TableName: MESSAGES_TABLE_NAME,
+      Item: {
+        messageId: v4(),
+        createdAt: new Date().getTime(),
+        nicknameTonickname: nicknameTonickname,
+        message: body.message,
+        sender: senderClient.nickname,
+      },
+    })
+    .promise();
+
+  const recipientConnectionId = await getConnectionIdByNickname(body.recipientNickname);
+
+  if (recipientConnectionId) {
+    await postToConnection(
+      recipientConnectionId,
+      JSON.stringify({
+        type: "message",
+        value: {
+          sender: senderClient.nickname,
+          message: body.message,
+        },
+      }),
+    );
+  }
+  return responseOK;
+};
+const getNicknameToNickname = (nicknames: string[]): string => nicknames.sort().join("#");
+const getClient = async (connectionId: string) => {
+  const output = await docClient
+    .get({
+      TableName: CLIENT_TABLE_NAME,
+      Key: {
+        connectionId: connectionId,
+      },
+    })
+    .promise();
+
+  return output.Item as Client;
+};
+
+const handleGetMessages = async (
+  connectionId: string,
+  body: GetMessagesBody,
+): Promise<APIGatewayProxyResult> => {
+  const client = await getClient(connectionId);
+  const output = await docClient
+    .query({
+      TableName: MESSAGES_TABLE_NAME,
+      IndexName: "NicknameTonicknameIndex",
+      KeyConditionExpression: "#nicknameToNickname = :nicknameToNickname",
+      ExpressionAttributeNames: {
+        "#nicknameToNickname": "nicknameToNickname",
+      },
+      ExpressionAttributeValues: {
+        ":nicknameToNickname": getNicknameToNickname([client.nickname, body.targetNickname]),
+      },
+      Limit: body.limit,
+      ExclusiveStartKey: body.startKey,
+      ScanIndexForward: false,
+    })
+    .promise();
+
+  const messages = output.Items && output.Items.length > 0 ? output.Items : [];
+
+  await postToConnection(
+    connectionId,
+    JSON.stringify({
+      type: "messages",
+      value: {
+        messages,
+      },
+    }),
+  );
+
+  return responseOK;
+};
